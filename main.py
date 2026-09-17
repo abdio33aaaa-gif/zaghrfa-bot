38612b4453a5d2dc750f7051aba5544b05a7eae8
abdio33aaaa-gif <abdio33aaaa@gmail.com>
2026-09-18 00:55:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import telebot, os
+from flask import Flask
+import threading
+
 TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=10000)
 
 @bot.message_handler(commands=['start'])
 def start(m):
@@ -11,4 +22,6 @@ def zag(m):
     t=m.text
     bot.send_message(m.chat.id, f"꧁{t}꧂\n★彡{t}彡★\n༺{t}༻\n『{t}』")
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    bot.infinity_polling()
